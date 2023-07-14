e7c19b9cc448bf8e2aecc0b4f15f2acc4e70acf2
rafam <rafam@LAPTOP-C06OBA9V>
2023-07-14 17:17:12 -0300

    Adicionar arquivos de projeto.

---
+++ Teste1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("digite a nota do primeiro semestre");
+
+float nota1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("digite a nota do segundo semestre");
+
+float nota2 = float.Parse(Console.ReadLine());
+
+
+float resultado = (nota1+nota2)/2;
+
+Console.WriteLine("sua media é de {0:N}\n", resultado);
+
